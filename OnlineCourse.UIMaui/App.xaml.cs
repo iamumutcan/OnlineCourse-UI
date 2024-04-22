e09c0361979b88b0409f6c42d3c986ed7c008bb3
Umut Can YILMAZ <umut31can@gmail.com>
2024-04-22 23:31:40 +0300

    Add project files.

---
+++ OnlineCourse.UIMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineCourse.UIMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ OnlineCourse.UIMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace OnlineCourse.UIMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
